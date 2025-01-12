7f30b69448545e2544336f262c185637e8b9eb57
basem <basem@DESKTOP-Q3US107>
2025-01-12 03:12:56 +0200

    setup3

--- TrialApp/WindowsFormsApp1/Form1.Designer.cs
+++ TrialApp/WindowsFormsApp1/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(367, 217);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
