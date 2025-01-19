1d2d20c1e6b06b3bc6457692025b925946dd25d0
basem <basem@DESKTOP-Q3US107>
2025-01-19 14:41:36 +0200

    setup10

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(292, 203);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Deployment.Application;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,7 +19,40 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AddIconToRemovePrograms("ts");
             Application.Run(new Form1());
         }
+        static string IconPath => Path.Combine(Application.StartupPath, "imgcompany.ico");
+
+        internal static void AddIconToRemovePrograms(string productName)
+    {
+        try
+        {   //first run after first install or after update
+            if (ApplicationDeployment.IsNetworkDeployed && ApplicationDeployment.CurrentDeployment.IsFirstRun)
+            {
+                if (File.Exists(IconPath))
+                {
+                    var uninstallKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+                    var subKeyNames = uninstallKey.GetSubKeyNames();
+                    for (int i = 0; i < subKeyNames.Length; i++)
+                    {
+                        using (var key = uninstallKey.OpenSubKey(subKeyNames[i], true))
+                        {
+                            var displayName = key?.GetValue("DisplayName");
+                            if (displayName == null || displayName.ToString() != productName)
+                                continue;
+                            //Set this to the display name of the application.
+                            key.SetValue("DisplayIcon", IconPath);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"We could not properly setup the application icons. Please notify your software vendor of this error.{Environment.NewLine}{ex.ToString()}", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
     }
 }
